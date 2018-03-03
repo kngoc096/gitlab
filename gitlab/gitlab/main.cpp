11b15597cc853a9a8d71b7250eca02ae53860270
kngoc096 <Ngoc.Nguyen@centria.fi>
2018-03-03 19:13:04 +0200

    commit gitlab main

---
+++ gitlab/gitlab/main.cpp
@@ -0,0 +1,25 @@
+//=============================================================================
+//	APPLICATION:	gitlab
+//	MODULE:			main
+//	PURPOSE:
+//	AUTHOR(S):		Dell
+//
+//	3/3/2018 7:07:11 PM	Created.
+//=============================================================================
+#include <iostream>
+using namespace std;
+//-----------------------------------------------------------------------------
+//	THE MAIN ENTRY POINT TO THE PROGRAM.
+//
+//	input:	argc	The number of arguments of the programme.
+//			argv	The vector of arguments of the programme.
+//	return:	The code of the reason the process was terminated for.
+//			The value defaults to zero.
+//-----------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+	cout << "My program in C++ Language." << endl;
+	cout << "This is my first lab" << endl;
+
+	return 0;
+}
